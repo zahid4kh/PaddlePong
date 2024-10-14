046ebb1340a7af5c50d4882c7d7bb4f41cc350b3
zahid4kh <halilzahid@gmail.com>
2024-10-14 21:47:10 +0200

    buttons

--- app/src/main/java/pong/clone/MainActivity.kt
+++ app/src/main/java/pong/clone/MainActivity.kt
@@ -19,8 +19,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pong.clone.ui.theme.PongCloneTheme
 
@@ -38,12 +41,17 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
+@Preview
 fun Lol() {
     val textMeasurer = rememberTextMeasurer()
     var playerX by remember { mutableFloatStateOf(0f) }
     var playerY by remember { mutableFloatStateOf(0f) }
     var botX by remember { mutableFloatStateOf(0f) }
     var botY by remember { mutableFloatStateOf(0f) }
+
+    //val pause = ImageBitmap.imageResource(id = R.drawable.pause_button)
+    val play = ImageBitmap.imageResource(id = R.drawable.play_button)
+
     Column(modifier = Modifier
         .fillMaxSize()
         .border(3.dp, Color.White, shape = RoundedCornerShape(40.dp))){
@@ -58,11 +66,17 @@ fun Lol() {
 
             botX = 40f
             botY = canvasHeight/2 - 170/2
-            val ballOffset = 72
+            //val ballOffset = 72
             drawText(textMeasurer, "Canvas size: ${Pair(canvasWidth, canvasHeight)}", topLeft = Offset(canvasWidth/2.4f, canvasHeight-100))
+
             drawCircle(Color.White, 50f, center = Offset(center.x, center.y))
+
             drawRoundRect(color = Color.White, size = Size(40f,170f), topLeft = Offset(playerX, playerY))
+
             drawRoundRect(color = Color.White, size = Size(40f,170f), topLeft = Offset(botX, botY))
+
+            drawImage(image = play, topLeft = Offset(300f, 300f))
+
         }
     }
 }
