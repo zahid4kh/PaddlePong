720134a363b74502978fb1a1959c757ade403982
zahid4kh <halilzahid@gmail.com>
2024-10-15 17:43:59 +0200

    ball is moving

--- app/src/main/java/pong/clone/MainActivity.kt
+++ app/src/main/java/pong/clone/MainActivity.kt
@@ -9,18 +9,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -54,10 +46,9 @@ fun Lol() {
     val configuration = context.resources.configuration
 
     val density = getScreenDensityDpi(context)
-
     val screenWidthDp = configuration.screenWidthDp
     val screenHeightDp = configuration.screenHeightDp
-
+//
     val screen = Pair((screenWidthDp * density) / 160, (screenHeightDp * density) / 160)
 
     val textMeasurer = rememberTextMeasurer()
@@ -67,14 +58,11 @@ fun Lol() {
     var botX by remember { mutableFloatStateOf(0f) }
     var botY by remember { mutableFloatStateOf(0f) }
 
-    var ballX by remember { mutableFloatStateOf((((screen.first)/2).toFloat())) }
-    var ballY by remember { mutableFloatStateOf((((screen.second)/2).toFloat())) }
-
-    var isUpdate by remember { mutableStateOf(false) }
-    if (isUpdate){
-        ballX += 1f
-        ballY += 1f
-    }
+    var ballX by remember { mutableFloatStateOf(screen.first.toFloat() / 2) }
+    var ballY by remember { mutableFloatStateOf(screen.second.toFloat() / 2) }
+    var speedX by remember { mutableFloatStateOf(5f) }
+    var speedY by remember { mutableFloatStateOf(5f) }
+    val ballRadius = 50f
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -93,23 +81,25 @@ fun Lol() {
             //val ballOffset = 72
             drawText(textMeasurer, "Canvas size: ${Pair(canvasWidth, canvasHeight)}", topLeft = Offset(canvasWidth/2.4f, canvasHeight-100))
 
-            drawCircle(Color.White, 50f, center = Offset(ballX, ballY))
-//            ballX += 1f
-//            ballY += 1f
+            ballX += speedX
+            ballY += speedY
+            if (ballX + ballRadius >= canvasWidth || ballX - ballRadius <= 0) {
+                speedX *= -1;
+            }
+
+            if (ballY + ballRadius >= canvasHeight || ballY - ballRadius <= 0){
+                speedY *= -1f;
+            }
+
+            drawCircle(Color.White, ballRadius, center = Offset(ballX, ballY))
+            println("Ball coordinates = ${Pair(ballX, ballY)}")
 
             drawRoundRect(color = Color.White, size = Size(40f,170f), topLeft = Offset(playerX, playerY))
 
             drawRoundRect(color = Color.White, size = Size(40f,170f), topLeft = Offset(botX, botY))
         }
-        val iconButton = if (isUpdate) Icons.Default.PlayArrow else Icons.Default.Refresh
-        IconButton(onClick = {isUpdate = !isUpdate}, modifier = Modifier.padding(start = (screenWidthDp/2).dp).size(50.dp,50.dp)){
-            Icon(iconButton, contentDescription = "Play Button", tint = Color.White, modifier = Modifier.size(50.dp,50.dp))
-        }
     }
 }
-
-
-
 
 
 
